25d9e777aadc8a8c7fd9137fcdedde0b4931a750
Suzy <suzy.nakayama@gmail.com>
2025-07-12 01:31:46 -0400

    feat: Home and Calulator Pages and logic added

---
+++ app/medicineCalculator/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from "expo-router";
+
+const MedicineCalculatorLayout = () => {
+	return (
+		<Stack
+			screenOptions={{
+				headerShown: false,
+			}}
+		/>
+	);
+};
+
+export default MedicineCalculatorLayout;
